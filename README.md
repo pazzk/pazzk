6240632f9e766f814c524f35d00317e02c5246d5
iqpizza6349 <iqpizza6349@gmail.com>
2024-03-03 11:40:56 +0900

    Create 'Pazzk' flow and how to use in README.md

--- README.md
+++ README.md
@@ -2,3 +2,30 @@
 
 치지직(chzzk)의 후원(치즈) 알림을 받아올 수 있는 Coroutine 기반 Kotlin API
 
+## Flow Chart
+![flow-chart.png](pazzk-flow.png)
+
+## How To Use
+
+```kotlin
+val pazzk = Pazzk("YOUR_DONATE_KEY")
+
+pazzk.addListener { response ->
+    if (response.isMessage) {
+        // which is websocket message
+        // such as connect, disconnect, error, etc...
+        println("$response.message")
+    }
+    else {
+        // which only for donate event
+        println("$reponse.amount 원 후원")
+    }
+}
+
+pazzk.connect()
+
+// something later...
+
+pazzk.disconnect()
+```
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    kotlin("plugin.serialization") version "1.9.22"
 }
 
 group = "io.pazzk"
@@ -11,6 +12,9 @@ repositories {
 
 dependencies {
     implementation("io.vertx:vertx-core:4.5.4")
+    implementation("org.slf4j:slf4j-api:2.0.12")
+    implementation("ch.qos.logback:logback-core:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.16.1")
