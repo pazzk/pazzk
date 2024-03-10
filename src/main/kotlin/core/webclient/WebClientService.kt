1c05aadd4ae6478fc5185c78c4ff9c4ebd875001
iqpizza6349 <iqpizza6349@gmail.com>
2024-03-10 15:39:53 +0900

    Make Default client to use on child(implemented) class

--- src/main/kotlin/core/webclient/WebClientService.kt
+++ src/main/kotlin/core/webclient/WebClientService.kt
@@ -1,9 +1,22 @@
 package io.pazzk.core.webclient
 
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.jackson.*
+
 interface WebClientService<T> {
 
     suspend fun get(): T?
 
     fun close()
+
+    companion object {
+        val DEFAULT_CLIENT = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                jackson()
+            }
+        }
+    }
 
 }
