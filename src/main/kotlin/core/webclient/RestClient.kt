ab67e94daf091c2d128e72fe569341b19e83e111
iqpizza6349 <iqpizza6349@gmail.com>
2024-03-10 15:41:12 +0900

    Add constructor client by default client

--- src/main/kotlin/core/webclient/RestClient.kt
+++ src/main/kotlin/core/webclient/RestClient.kt
@@ -3,12 +3,13 @@ package io.pazzk.core.webclient
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import io.pazzk.core.webclient.WebClientService.Companion.DEFAULT_CLIENT
 import io.pazzk.json.Session
 import org.slf4j.LoggerFactory
 
 class RestClient(
     private val donationId: String,
-    private val client: HttpClient
+    private val client: HttpClient = DEFAULT_CLIENT
 ) : WebClientService<Session> {
     companion object {
         private const val host = "https://api.chzzk.naver.com/manage/v1/alerts"
